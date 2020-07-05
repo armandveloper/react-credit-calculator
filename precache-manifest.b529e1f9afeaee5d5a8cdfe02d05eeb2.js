0d67a27f69a3275ffa4f04df06e6477d440d701e
Armando <jaimes087.uz@gmail.com>
2020-07-05 00:20:45 -0500

    Updates

---
+++ precache-manifest.b529e1f9afeaee5d5a8cdfe02d05eeb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f013848b412822d28bf570fa75e02877",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c503953c2725a52df4be",
+    "url": "/static/css/main.e94aa8db.chunk.css"
+  },
+  {
+    "revision": "8c9494dd74f6d373dc07",
+    "url": "/static/js/2.457f9a6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.457f9a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c503953c2725a52df4be",
+    "url": "/static/js/main.2d284a0b.chunk.js"
+  },
+  {
+    "revision": "8701f25ecb56b196bf69",
+    "url": "/static/js/runtime-main.963f31a7.js"
+  }
+]);
